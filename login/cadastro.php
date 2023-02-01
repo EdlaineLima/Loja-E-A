1611dad08014d83a7d6fdd6e1b837429f2aeaa9d
Edlaine Lima <edlaine.lima.lima@gmail.com>
2023-02-01 10:14:12 -0300

    login atualizado

--- login/cadastro.php
+++ login/cadastro.php
@@ -4,48 +4,65 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Tela de Login</title>
 </head>
+
 <body>
-    
-    <div class="ok">
-            <?php
-                if(isset($_SESSION['status_cadastro'])):
-            ?> 
-            <div>
-            <p>Cadastro efetuado!
-            Faça login informando o seu usuário e senha <a href="login.php"><span>aqui</span></a></p>
-        </div>
-            <?php
-                endif;
-                unset($_SESSION['status_cadastro']);
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="../index.html">Home</a>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+    <div id="container">
+        <?php
+        if (isset($_SESSION['status_cadastro'])):
             ?>
-            
-           <?php
-                if(isset($_SESSION['usuario_existe'])):
-            ?>
             <div>
-                <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
+                <p style="color: green;">Cadastro efetuado!
+                    Faça login informando o seu usuário e senha <a href="login.php"><span>aqui</span></a></p>
             </div>
             <?php
-                endif;
-                unset($_SESSION['usuario_existe']);
-            ?>
-            <h1>CADASTRO</h1>
+        endif;
+        unset($_SESSION['status_cadastro']);
+        ?>
 
-        <form action="cadastrar.php" method="POST">
-            
-            <input type="text" name="nome" placeholder="Nome">
-            <input type="text" name="usuario" placeholder="Usuario">
-            <input type="password" name="senha" placeholder="Senha">
-            
-            <button type="submit">Enviar</button>
-        </form>
+        <?php
+        if (isset($_SESSION['usuario_existe'])):
+            ?>
+            <div>
+                <p style="color: red;">O usuário escolhido já existe. Informe outro e tente novamente.</p>
+            </div>
+            <?php
+        endif;
+        unset($_SESSION['usuario_existe']);
+        ?>
+
+        <div class="form-login">
+
+            <h1 class="titulo-cad">CADASTRO</h1>
+
+            <form action="cadastrar.php" method="POST">
+                <input type="text" name="nome" placeholder="Nome">
+                <input type="text" name="usuario" placeholder="Usuario">
+                <input type="password" name="senha" placeholder="Senha">
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </html>
--- login/index.php
+++ login/index.php
@@ -4,44 +4,65 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
     <title>Tela de Login</title>
 </head>
+
 <body>
-       
-    <div class="ok">
+
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="../index.html">Home</a>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container">
         <div>
             <?php
-                if(isset($_SESSION['nao_autenticado'])):
-            ?> 
-        
-            <p>ERRO: Usuário ou senha Inválidos.</p>
-        </div>
-            <?php
-                endif;
-                unset($_SESSION['nao_autenticado']);
+            if (isset($_SESSION['nao_autenticado'])):
+                ?>
+
+                <p>ERRO: Usuário ou senha Inválidos.</p>
+
+                <?php
+            endif;
+            unset($_SESSION['nao_autenticado']);
             ?>
-        
+        </div>
 
-        <h1>LOGIN</h1>
-        
-        <form action="login.php" method="POST">
-            
-          <input type="text" name="usuario" placeholder="Usuario">
-          <input type="password" name="senha" placeholder="Senha">
+        <div class="form-login">
 
-          <button type="submit">Entrar</button>
+            <h1 class="titulo-login">LOGIN</h1>
 
-        </form>
+            <form action="login.php" method="POST">
 
-        <span>Não é Cadastrado?</span><a class="cad" href="cadastro.php">Cadastre-se</a>
+                <input type="text" name="usuario" placeholder="Usuario">
+
+                <input type="password" name="senha" placeholder="Senha">
+
+                <button type="submit">Entrar</button>
+            </form>
+
+            <span>Não é Cadastrado?</span><a class="cadastro" href="cadastro.php">Cadastre-se</a>
+        </div>
+
+
 
     </div>
-        
-    
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
 </html>
